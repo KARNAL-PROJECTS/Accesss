03fa3992cb948136081daec265bef2fff5bc2683
KARNAL-PROJECTS <seraza934@gmail.com>
2026-04-02 06:35:01 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -215,3 +215,13 @@ def lgOut():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API WORKING"
+
+handler = app
